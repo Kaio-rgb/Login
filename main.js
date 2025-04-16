23143442f38324964dea0997c885ece922357f46
Kaio-rgb <paiterschobiner@gmail.com>
2025-04-15 23:55:40 -0300

    adicionei o codigo java em sala com o professor

---
+++ main.js
@@ -0,0 +1,26 @@
+// Implementação acesso para a tela de login
+function acessar() {
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+
+    //implementa estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha) {
+        alert("Favor preencher todos os campos");
+    } else {
+        window.location.href = "cadastro.html";
+    }
+}
+//cria variavel tipo array
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+    dadosLista.push(nomeUser);
+    console.log(dadosLista);
+    document.getElementById('nomeUser').value = "";
+
+
+    }
+}
